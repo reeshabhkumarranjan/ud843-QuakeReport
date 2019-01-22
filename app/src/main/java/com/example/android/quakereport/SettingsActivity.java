be09157a6f2239562bc0b85810beba702828e510
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-01-23 00:49:13 +0530

    Started with adding settings menu. (WUP) (errors in the project)

---
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.quakereport;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class QuakePreferenceFragment extends PreferenceFragment{
+
+    }
+}
